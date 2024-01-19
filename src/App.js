72f69b4e823b122980ea1c65c33876b4c76574c9
Abel Sassu <relinquished@hotmail.it>
2024-01-19 16:50:24 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Col, Container , Row} from "react-bootstrap";
+import MainPage from "./components/MainPage";
+import ArtistPage from "./components/ArtistPage";
+import AlbumPage from "./components/AlbumPage";
+import SearchPage from "./components/SearchPage";
+import MediaPlayer from "./components/MediaPlayer";
+import MySidebar from "./components/MySidebar";
+import MyLibrary from "./components/MyLibrary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => (
+  <>
+  <BrowserRouter>
+    <Container className="mx-0">
+      <Row>
+      
+        <Col xs={2}>
+           <MySidebar/> 
+        </Col>
+        <Col xs={10}>
+          
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/artist" element={<ArtistPage/>}/>
+            <Route path="/album/:id" element={<AlbumPage/>}/>
+            <Route path="/search/:query" element={<SearchPage/>}/>
+            <Route path="/mylibrary" element={<MyLibrary/>} />
+          </Routes>
+          
+        </Col>  
+      </Row>
+      <MediaPlayer />
+      
+    </Container>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
